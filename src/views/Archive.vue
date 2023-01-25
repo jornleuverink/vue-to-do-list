dafa04d0f3adf30e846fdb0a060316dffa2254c1
Jorn Leuverink <110493468+jornleuverink@users.noreply.github.com>
2023-01-25 15:14:53 +0100

    Add files via upload

---
+++ src/views/Archive.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>To do List:</h1>
+    <TodoList location="archive" />
+  </div>
+</template>
+
+<script>
+import TodoList from "../components/TodoList.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    TodoList,
+  },
+};
+</script>
